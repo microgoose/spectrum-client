7087af3fde552688456b812942637cb3106549df
microgoose <gusakmachine@yandex.ru>
2025-07-23 21:16:44 +0300

    feat: добавлено разбитие по чанкам для библиотек

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,50 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ mode }) => {
+  const plugins = [
     vue(),
     vueDevTools(),
     svgLoader({ defaultImport: 'component' }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  ]
+
+  if (mode === 'analyze') { // Для визуального отображения размера чанков
+    plugins.push(
+      visualizer({
+        open: true,
+        filename: 'dist/stats.html',
+        gzipSize: true,
+        brotliSize: true,
+      })
+    )
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) { // Разбиваем библиотеки на чанки
+            if (id.includes('node_modules')) {
+              if (id.includes('primevue')) return 'primevue'
+              if (id.includes('primeicons')) return 'primeicons'
+              if (id.includes('@tanstack')) return 'vue-query'
+              if (id.includes('vue-i18n')) return 'vue-i18n'
+              if (id.includes('vue-router')) return 'vue-router'
+              if (id.includes('@zod')) return 'zod'
+              return 'vendor'
+            }
+          },
+        },
+      },
+    },
+  }
 })
